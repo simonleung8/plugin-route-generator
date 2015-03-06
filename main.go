ab48e99047a3ee66565d62e34e6c4ead8127034e
Simon Leung <leungs@us.ibm.com>
2015-03-06 15:57:14 -0800

    add get-apps command

--- main.go
+++ main.go
@@ -23,47 +23,48 @@ func main() {
 func (g *GenRoutes) Run(cliConnection plugin.CliConnection, args []string) {
 	if args[0] == "gen-routes" {
 		///////////////////////////////////
-		i, err := strconv.ParseInt(args[1], 10, 64)
-		if err != nil {
-			fmt.Println("Num of route is invalid:", err)
-			os.Exit(1)
-		}
-		fmt.Println(i)
-
-		for x := 0; x < int(i); x++ {
-			// go cliConnection.CliCommand("files", "plugins")
-			go cliConnection.CliCommand("app", "plugins", "--guid")
-		}
-
-		go cliConnection.CliCommand("app", "cli")
-		c := make(chan int)
-		<-c
-		///////////////////////////////////
-		// if len(args) != 7 {
-		// 	fmt.Println("Invalid Usage: cf gen-route APP_NAME SPACE DOMAIN HOST NUM_ROUTE OFFSET")
-		// 	os.Exit(1)
-		// }
-
-		// i, err := strconv.ParseInt(args[5], 10, 64)
+		// i, err := strconv.ParseInt(args[1], 10, 64)
 		// if err != nil {
 		// 	fmt.Println("Num of route is invalid:", err)
 		// 	os.Exit(1)
 		// }
-		// offset, err := strconv.ParseInt(args[6], 10, 64)
-		// if err != nil {
-		// 	fmt.Println("Num of offset is invalid:", err)
-		// 	os.Exit(1)
+		// fmt.Println(i)
+
+		// for x := 0; x < int(i); x++ {
+		// 	// go cliConnection.CliCommand("files", "plugins")
+		// 	go cliConnection.CliCommand("files", "plugins", "app/repo-index.yml")
 		// }
 
-		// fmt.Printf("Generating %d routes for '%s'...\n\n", i, args[2])
+		// go cliConnection.CliCommand("app", "cli")
+		// c := make(chan int)
+		// <-c
+		///////////////////////////////////
+		if len(args) != 7 {
+			fmt.Println("Invalid Usage: cf gen-route APP_NAME SPACE DOMAIN HOST NUM_ROUTE OFFSET")
+			os.Exit(1)
+		}
 
-		// for x := int(offset); x < int(i+offset); x++ {
-		// 	cliConnection.CliCommand("map-route", args[1], args[3], "-n", args[4]+strconv.Itoa(x))
-		// }
+		i, err := strconv.ParseInt(args[5], 10, 64)
+		if err != nil {
+			fmt.Println("Num of route is invalid:", err)
+			os.Exit(1)
+		}
+		offset, err := strconv.ParseInt(args[6], 10, 64)
+		if err != nil {
+			fmt.Println("Num of offset is invalid:", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Generating %d routes for '%s'...\n\n", i, args[2])
+
+		for x := int(offset); x < int(i+offset); x++ {
+			cliConnection.CliCommand("create-route", args[2], args[3], "-n", args[4]+strconv.Itoa(x))
+			// cliConnection.CliCommand("map-route", args[1], args[3], "-n", args[4]+strconv.Itoa(x))
+		}
 
 	} else if args[0] == "del-routes" {
-		if len(args) != 4 {
-			fmt.Println("Invalid Usage: cf del-route DOMAIN HOST NUM_ROUTE")
+		if len(args) != 5 {
+			fmt.Println("Invalid Usage: cf del-route DOMAIN HOST NUM_ROUTE OFFSET")
 			os.Exit(1)
 		}
 
@@ -73,10 +74,33 @@ func (g *GenRoutes) Run(cliConnection plugin.CliConnection, args []string) {
 			os.Exit(1)
 		}
 
+		offset, err := strconv.ParseInt(args[4], 10, 64)
+		if err != nil {
+			fmt.Println("Num of offset is invalid:", err)
+			os.Exit(1)
+		}
+
 		fmt.Printf("deleting routes ...\n\n")
 
-		for x := 0; x < int(i); x++ {
+		for x := int(offset); x < int(i+offset); x++ {
 			cliConnection.CliCommand("delete-route", args[1], "-n", args[2]+strconv.Itoa(x), "-f")
+		}
+
+	} else if args[0] == "get-apps" {
+		if len(args) != 2 {
+			fmt.Println("Invalid Usage: cf get-apps NUM_OFF_RUN")
+			os.Exit(1)
+		}
+
+		i, err := strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			fmt.Println("Num of run is invalid:", err)
+			os.Exit(1)
+		}
+
+		for x := 0; x < int(i); x++ {
+			fmt.Println("RUN #", x)
+			cliConnection.CliCommand("apps")
 		}
 
 	}
@@ -104,7 +128,15 @@ func (g *GenRoutes) GetMetadata() plugin.PluginMetadata {
 				Alias:    "dr",
 				HelpText: "Delete generated routes for an app",
 				UsageDetails: plugin.Usage{
-					Usage: "cf del-route DOMAIN HOST NUM_ROUTE",
+					Usage: "cf del-route DOMAIN HOST NUM_ROUTE OFFSET",
+				},
+			},
+			{
+				Name:     "get-apps",
+				Alias:    "ga",
+				HelpText: "repeatedly run `apps` command",
+				UsageDetails: plugin.Usage{
+					Usage: "cf get-apps NUM_OFF_RUN",
 				},
 			},
 		},
